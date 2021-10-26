766dbe0045943daee9bc89596aa3993072821158
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-10-26 20:07:19 +0200

    + <HomePage /> + router installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {React, useState} from 'react';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Info from "./components/Info";
 
 export default function App() {
@@ -21,24 +21,29 @@ export default function App() {
   }
 
   return (
-      <div className="container-fluid">
-          <div
-              className={`navbar .form-inline bg-secondary pt-2 mt-n1 my-1 rounded justify-content-${windowSize > 610 ? "between" : "center"}`}
-              style={{position: "fixed", top: "0", left: "15px", right: "15px", zIndex: 1}}
-          >
-              <div>
-                  <span className="navbar-brand mb-0 mx-3 h1 text-white">
-                      <i className="bi bi-github"></i> GitHub API Users Search App
-                  </span>
-              </div>
-              <div>
-                  <span><input type="text" placeholder="input github user's name" className="me-3" onChange={handleChange}></input></span>
-                  <button type="button" className="btn btn-primary me-3" onClick={handleClick}><i className="bi bi-search"></i></button>
-              </div>
-          </div>
-          <div style={{marginTop: `${windowSize > 610 ? "70px" : "110px"}`}}>
-              <Info userName={userName} />
-          </div>
-      </div>
+    <BrowserRouter>
+        <div className="container-fluid">
+            <div
+                className={`navbar .form-inline bg-secondary pt-2 mt-n1 my-1 rounded justify-content-${windowSize > 610 ? "between" : "center"}`}
+                style={{position: "fixed", top: "0", left: "15px", right: "15px", zIndex: 1}}
+            >
+                <div>
+                    <span className="navbar-brand mb-0 mx-3 h1 text-white">
+                        <i className="bi bi-github"></i> GitHub API Users Search App
+                    </span>
+                </div>
+                <div>
+                    <span><input type="text" placeholder="input github user's name" className="me-3" onChange={handleChange}></input></span>
+                    <button type="button" className="btn btn-primary me-3" onClick={handleClick}><i className="bi bi-search"></i></button>
+                </div>
+            </div>
+            
+
+            
+            <div style={{marginTop: `${windowSize > 610 ? "70px" : "110px"}`}}>
+                <Info userName={userName} />
+            </div>
+        </div>
+    </BrowserRouter>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,29 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>What is  GitHub API Users Searching App?</h1>
+            <p>This app allows you to find & view any user's basic profile info & public repositories published on GitHub!</p>
+            <h3>How to use this App?</h3>
+            <p>Input the correct GitHub User username in search field (or add it after slash to url) & you will see:</p>
+            <ul>
+                <li>user's current bio & location</li>
+                <li>the number of user's open source projects (public repositories)</li>
+                <li>user's projects' detailes: description, language & link to project's github pages if available or source code if not ;-)</li>
+            </ul>
+            <h3>Technologies used in this project:</h3>
+            <ul>
+                <li>React</li>
+                <li>React Router</li>
+                <li>Bootstrap</li>
+                <li>GitHub API</li>
+                <li>gh-pages deployment</li>
+            </ul>
+            <h5>Note:</h5>
+            <p>
+                This is the newest version of the app.
+                <a href="https://github.com/vadimgierko/GitHub-Users-Search-App">Previous version</a> was built without using create-react-app.
+                Current version is built with create-react-app & gh-pages deployment.
+            </p>
+        </div>
+    );
+}
